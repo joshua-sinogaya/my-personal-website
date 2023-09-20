b6c19850ad9b75763d9ba1eb89d04138841bd4ee
joshua-sinogaya <74108440+joshua-sinogaya@users.noreply.github.com>
2023-09-20 21:31:15 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/header";
+import Hero from "./pages/hero/hero";
+import Projects from "./pages/projects/projects";
+
+function App() {
+	return (
+		<>
+			<h1>Currently updating! Kindly check back shortly.</h1>
+		</>
+	);
+}
+
+export default App;
