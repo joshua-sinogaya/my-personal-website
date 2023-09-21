c4832908491cb2dad0966ce3509eecc30f020901
joshua-sinogaya <74108440+joshua-sinogaya@users.noreply.github.com>
2023-09-21 23:01:15 +0800

    update hero.jsx
    yet another update

--- src/pages/hero/hero.jsx
+++ src/pages/hero/hero.jsx
@@ -3,9 +3,9 @@ import silpe from "../../assets/silpe2.jpg";
 const Hero = () => {
 	return (
 		<>
-			<div className='hero min-h-screen overflow-x-hidden min-w-full bg-base-100'>
+			<div className='hero min-h-screen overflow-x-hidden bg-base-100'>
 				<div className='hero-content flex-col'>
-					<img src={silpe} alt='myImage' className='max-w-lg' />
+					<img src={silpe} alt='myImage' className='w-full max-w-lg' />
 				</div>
 			</div>
 		</>
